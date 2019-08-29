d1e779b38ab4812308789a792dee779a9b1b13dd
Raja Paul <rajpaul@deloitte.com>
2019-08-29 16:49:07 +0530

    ng nuild updated

--- src/main/resources/static/styles.js
+++ src/main/resources/static/styles.js
@@ -555,7 +555,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ashutjha\Documents\FI\data loader\DataLoadEngine-SpringBootUi\src\main\springbot-angularApp\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\rajpaul\Desktop\ProjectData\ASHUTOSH\DataLoadEngine-SpringBootUi\src\main\springbot-angularApp\src\styles.css */"./src/styles.css");
 
 
 /***/ })
